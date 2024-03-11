b30f1f483a54e648d6f0046342971d0dc0774fc4
Felix Gradinaru <felix@gradinaru.me>
2024-03-11 22:44:21 +0100

    FEATURE: Add role that disables simplified publish button and allows workspace selection to editor

--- Resources/Private/NeosUserInterface/src/manifest.js
+++ Resources/Private/NeosUserInterface/src/manifest.js
@@ -11,64 +11,69 @@ import {useSelector, useDispatch} from 'react-redux';
 import PublishDropDown from './neos-ui/PublishDropDown'
 
 const Modal = (({iframeUri}) => {
-	const isOpen = useSelector(selectors.advancedPublishDialogOpen);
-	const iframeRef = React.useRef(null);
-	const dispatch = useDispatch();
+    const isOpen = useSelector(selectors.advancedPublishDialogOpen);
+    const iframeRef = React.useRef(null);
+    const dispatch = useDispatch();
 
-	const setExit = React.useCallback(() => {
-		const iframeWindow = iframeRef.current?.contentWindow;
-		if (!iframeWindow) {
-			return;
-		}
+    const setExit = React.useCallback(() => {
+        const iframeWindow = iframeRef.current?.contentWindow;
+        if (!iframeWindow) {
+            return;
+        }
         window.addEventListener('message', (event) => {
             if (event.data === 'closePublicationPopup') {
                 dispatch(actions.toggleAdvancedPublishDialog({open: false}));
             }
         })
-	}, [])
+    }, [])
 
-	if (!isOpen) {
-		return "";
-	}
+    if (!isOpen) {
+        return "";
+    }
 
-	return <iframe
-		ref={iframeRef}
-		onLoad={setExit}
-		style={{
-			width: "100%",
-			height: "100%",
-			position: "absolute",
-			zIndex: "999999",
-			top: "0",
-			background: "#000",
-			border: "0"
-		}}
-		src={iframeUri}
-	/>;
+    return <iframe
+        ref={iframeRef}
+        onLoad={setExit}
+        style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            zIndex: "999999",
+            top: "0",
+            background: "#000",
+            border: "0"
+        }}
+        src={iframeUri}
+    />;
 })
 
 manifest("CodeQ.AdvancedPublish", {}, (globalRegistry, {frontendConfiguration}) => {
-  if (!frontendConfiguration["CodeQ.AdvancedPublish"].enabled) {
-    return
-  }
+    const frontendConfigurationElement: {
+        enabled: boolean,
+        isSuperEditor: boolean,
+        iframeUri: string
+    } = frontendConfiguration["CodeQ.AdvancedPublish"];
+    if (!frontendConfigurationElement.enabled || frontendConfigurationElement.isSuperEditor === true) {
+        return
+    }
 
-	function* afterPublish() {
-		yield takeLatest(actionTypes.CR.Workspaces.PUBLISH, function* () {
-			const {payload} = yield take(actionTypes.ServerFeedback.HANDLE_SERVER_FEEDBACK)
-			if (!payload.feedbackEnvelope.feedbacks.some((feedback) => feedback.type === "Neos.Neos.Ui:Success")) {
-				console.warn(`CodeQ.AdvancedPublish :: Publishing doesnt seem to have been successful. Aborting.`)
-				return;
-			}
-			yield put(actions.toggleAdvancedPublishDialog())
-		})
-	}
+    function* afterPublish() {
+        yield takeLatest(actionTypes.CR.Workspaces.PUBLISH, function* () {
+            const {payload} = yield take(actionTypes.ServerFeedback.HANDLE_SERVER_FEEDBACK)
+            if (!payload.feedbackEnvelope.feedbacks.some((feedback) => feedback.type === "Neos.Neos.Ui:Success")) {
+                console.warn(`CodeQ.AdvancedPublish :: Publishing doesnt seem to have been successful. Aborting.`)
+                return;
+            }
+            yield put(actions.toggleAdvancedPublishDialog())
+        })
+    }
 
-	globalRegistry.get('containers').set('Modals/CodeQ.AdvancedPublish', () => ReactDOM.createPortal(
-		<Modal iframeUri={frontendConfiguration["CodeQ.AdvancedPublish"].iframeUri}/>,
-		document.body
-	));
-	globalRegistry.get('sagas').set('CodeQ.AdvancedPublish/afterPublish', { saga: afterPublish });
-	globalRegistry.get('reducers').set('CodeQ.AdvancedPublish', { reducer });
+    globalRegistry.get('containers').set('Modals/CodeQ.AdvancedPublish', () => ReactDOM.createPortal(
+        <Modal iframeUri={frontendConfigurationElement.iframeUri}/>,
+        document.body
+    ));
+    globalRegistry.get('sagas').set('CodeQ.AdvancedPublish/afterPublish', {saga: afterPublish});
+    globalRegistry.get('reducers').set('CodeQ.AdvancedPublish', {reducer});
 
-	globalRegistry.get('containers').set('PrimaryToolbar/Right/PublishDropDown', PublishDropDown);
+    globalRegistry.get('containers').set('PrimaryToolbar/Right/PublishDropDown', PublishDropDown);
 });
